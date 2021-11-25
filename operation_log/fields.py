a0420a843c3cfbd56fdef75ea5ac3b4d235dda97
谭浩文 <tanhw2@yonghui.cn>
2021-11-25 15:16:15 +0800

    rm

--- operation_log/fields.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ipaddress import IPv4Address
-
-
-class IPAddressField(serializers.IPAddressField):
-
-    def to_representation(self, value):
-        return super().to_representation(
-            value.ip if isinstance(value, IPv4Address) else value)
