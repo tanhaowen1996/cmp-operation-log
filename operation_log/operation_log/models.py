acd29755db0a5b6be3fde2709f23b78478d81447
谭浩文 <tanhw2@yonghui.cn>
2022-03-03 18:10:11 +0800

    change time

--- operation_log/operation_log/models.py
+++ operation_log/operation_log/models.py
@@ -1,13 +1,8 @@
-from django.conf import settings
-from django.contrib.auth.models import Group
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres.indexes import BrinIndex
-import requests
-import json
 import uuid
-from .utils import PikaMixin
-import pika
 
 
 class OperationLog(models.Model):
@@ -57,3 +52,4 @@ class OperationLog(models.Model):
 
     class Meta:
         indexes = (BrinIndex(fields=['created_at']),)
+        ordering = ('-created_at',)
