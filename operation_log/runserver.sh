8c04499b96dcada4a2ba61397f92e695fcc29839
谭浩文 <tanhw2@yonghui.cn>
2022-03-07 16:13:18 +0800

    change model

--- operation_log/runserver.sh
+++ operation_log/runserver.sh
@@ -9,7 +9,7 @@ WEB_CONCURRENCY=${WEB_CONCURRENCY:=3}
 MAX_REQUESTS=${MAX_REQUESTS:=5000}
 
 if [ "asgi" = "$SERVICE_MODE" ]; then
-    gunicorn djapp.asgi -b :$WEB_PORT -k uvicorn.workers.UvicornWorker -w $WEB_CONCURRENCY --max-requests $MAX_REQUESTS
+    gunicorn operation_log.asgi -b :$WEB_PORT -k uvicorn.workers.UvicornWorker -w $WEB_CONCURRENCY --max-requests $MAX_REQUESTS
 elif [ "django" = "$SERVICE_MODE" ]; then
     ./manage.py runserver 0:$WEB_PORT
 elif [ "operation_log" = "$SERVICE_MODE" ]; then
